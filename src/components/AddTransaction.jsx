d943557c034c189a957b33102def009ba67d9b7e
Shamin Yasar <113526109+shamin2@users.noreply.github.com>
2024-04-04 13:59:45 -0230

    Finishing the transaction module

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -13,5 +13,29 @@ export const AddTransaction = () => {
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
       text,
-      amount: +amount
+      amount: +amount //parsing this to integer value
     }
+
+    addTransaction(newTransaction);
+  }
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter your amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -16,3 +16,15 @@ function moneyFormatter(num) {
     p[1]
   );
 }
+
+export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+  
+    const sign = transaction.amount < 0 ? '-' : '+';
+  
+    return (
+      <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+        {transaction.text} <span>{sign}{moneyFormatter(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+      </li>
+    )
+}
