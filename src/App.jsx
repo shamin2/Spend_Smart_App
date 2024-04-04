0aaf82a4e79da06ac336a625555dbed29162439d
Shamin Yasar <113526109+shamin2@users.noreply.github.com>
2024-04-04 03:21:32 -0230

    Working with App jsx module, and using the context Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
-export const App = () => {
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
 }
+
+export default App;
